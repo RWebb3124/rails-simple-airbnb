5a700dffffcdaf0ce20ff73290f5279d90edb54e
Ryan Webb <rwebb3124@gmail.com>
2024-07-03 15:39:42 +0100

    Index completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.destroy_all
+
+20.times do
+  Flat.create!(
+    {
+      name: "#{rand(1..5)} bed flat in #{Faker::Address.city}",
+      address: Faker::Address.full_address,
+      description: Faker::Lorem.paragraph(sentence_count: 4),
+      price_per_night: rand(80..1000),
+      number_of_guests: rand(1..6)
+    }
+  )
+end
